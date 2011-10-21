f1054437b6f7374b9d4a4abde2bf2f89b115fc4a
David Monllaó <david.monllao@gmail.com>
2011-10-21 13:52:11 +0200

    MINOR - Adding maturity and release values

--- version.php
+++ version.php
@@ -1,12 +1,8 @@
 <?php
 
-////////////////////////////////////////////////////////////////////////////////
-//  Code fragment to define the module version etc.
-//  This fragment is called by /admin/index.php
-////////////////////////////////////////////////////////////////////////////////
-
-$module->version  = 2011072100;
-$module->requires = 2010092100;  // Requires this Moodle version
+$module->version  = 2011102100;
+$module->requires = 2010112400;  // Requires this Moodle version
+$module->release = '1.0 (Build: 2011102100)';
+$module->maturity = MATURITY_STABLE;
 $module->cron     = 60;
 
-?>
