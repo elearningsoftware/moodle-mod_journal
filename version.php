a0a667416a294ae7f19f8ae59d6d28435280f2d3
David Monllaó <david.monllao@gmail.com>
2012-03-20 14:00:07 +0100

    CONTRIB-3537 - Changing version release

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 
 $module->version  = 2012032001;
 $module->requires = 2010112400;  // Requires this Moodle version
-$module->release = '1.1 (Build: 2011110500)';
+$module->release = '1.2 (Build: 2012032001)';
 $module->maturity = MATURITY_STABLE;
 $module->cron     = 60;
 
