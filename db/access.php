a10bd7e8519f357dbab914ecd3addc6e8dfdc12f
David Monllaó <david.monllao@gmail.com>
2012-04-17 09:02:45 +0200

    CONTRIB-3550 - Removing the admin from journal:addentries default users

--- db/access.php
+++ db/access.php
@@ -21,8 +21,7 @@ $capabilities = array(
         'archetypes' => array(
             'student' => CAP_ALLOW,
             'teacher' => CAP_ALLOW,
-            'editingteacher' => CAP_ALLOW,
-            'manager' => CAP_ALLOW
+            'editingteacher' => CAP_ALLOW
         )
     )
     
