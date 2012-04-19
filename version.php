a9a25fa0da7748b6c8ac058fc60b7ce436927157
David Monllaó <david.monllao@gmail.com>
2012-04-19 16:54:55 +0200

    CONTRIB-2791 - New version

--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?php
 
-$module->version  = 2012032001;
+$module->version  = 2012041900;
 $module->requires = 2010112400;  // Requires this Moodle version
-$module->release = '1.2 (Build: 2012032001)';
+$module->release = '1.3 (Build: 2012041900)';
 $module->maturity = MATURITY_STABLE;
 $module->cron     = 60;
 
