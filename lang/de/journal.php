e362663146186d9580c55ed067d96ef4107903ed
David Monllaó <david.monllao@gmail.com>
2012-04-19 15:53:11 +0200

    CONTRIB-2791 - Added German translation, contribution by Witzig Samuel

---
+++ lang/de/journal.php
@@ -0,0 +1,41 @@
+﻿<?php
+
+$string['accessdenied'] = 'Zugriff verboten';//Access denied
+$string['alwaysopen'] = 'Immer geöffnet';//Always open, 1.9
+$string['blankentry'] = 'Leerer Eintrag';//Blank entry, 1.9
+$string['daysavailable'] = 'Verfügbare Tage';//Days available, 1.9
+$string['editingended'] = 'Bearbeitungszeitraum ist abgelaufen';//Editing period has ended, 1.9
+$string['editingends'] = 'Bearbeitungszeitraum abgelaufen';//Editing period ends, 1.9
+$string['entries'] = 'Einträge';//Entries, 1.9
+$string['feedbackupdated'] = 'Rückmeldung für {$a} Einträge aktualisiert';//Feedback updated for {$a} entries, 1.9
+$string['gradeingradebook'] = 'Aktuelle Bewertung in der Bewertungsübersicht';//Current grade in gradebook
+$string['journal:addentries'] = 'Journaleinträge erstellen';//Add journal entries
+$string['journal:manageentries'] = 'Journaleinträge verwalten';//Manage journal entries
+$string['journalmail'] = '{$a->teacher} hat eine Rückmeldung zu Ihrem Eintrag im Journal \'{$a->journal}\' abgegeben.
+
+Sie können diese Rückmeldung als Anhang an Ihrem Journaleintrag einsehen:
+
+    {$a->url}';
+$string['journalmailhtml'] = '{$a->teacher} hat eine Rückmeldung zu Ihrem Eintrag im Journal \'<i>{$a->journal}</i>\' abgegeben.<br /><br />Sie können diese Rückmeldung als Anhang an Ihrem <a href=\"{$a->url}\">Journaleintrag</a> ansehen.';
+$string['journalname'] = 'Name';//Journal name
+$string['journalquestion'] = 'Zusammenfassung';//Journal question, 1.9
+$string['mailsubject'] = 'Rückmeldung zu Journaleintrag';//Journal feedback
+$string['modulename'] = 'Journal';
+$string['modulenameplural'] = 'Journale';//Journals
+$string['newjournalentries'] = 'Neue Journaleinträge';//New journal entries, 1.9
+$string['noentriesmanagers'] = 'Keine Trainer eingeschrieben';//There are no teachers
+$string['noentry'] = 'Kein Eintrag';//No entry, 1.9
+$string['noratinggiven'] = 'Keine Bewertung abgegeben';//No rating given, 1.9
+$string['notopenuntil'] = 'Dieses Journal ist geschlossen bis';//This journal won\'t be open until, 1.9
+$string['notstarted'] = 'Sie haben mit Ihrem Journal noch nicht begonnen';//You have not started this journal yet, 1.9
+$string['overallrating'] = 'Allgemeine Bewertung';//Overall rating, 1.9
+$string['pluginadministration'] = 'Administration für das Journal-Modul';//Journal module administration
+$string['pluginname'] = 'Journal';
+$string['rate'] = 'Bewertung';//Rate, 1.9
+$string['removeentries'] = 'Alle Journaleinträge löschen';//Remove all entries
+$string['saveallfeedback'] = 'Alle meine Rückmeldungen speichern';//Save all my feedback, 1.9
+$string['showrecentactivity'] = 'Aktuelle Aktivitäten anzeigen';//Show recent activity
+$string['showoverview'] = 'Journalübersicht unter meine Startseite anzeigen';//Show journals overview on my moodle
+$string['startoredit'] = 'Journaleintrag beginnen oder bearbeiten';//Start or edit my journal entry. 1.9
+$string['viewallentries'] = 'Zeige {$a} Journaleinträge';//View {$a} journal entries, 1.9
+$string['viewentries'] = 'Zeige Journaleinträge';//View entries, 1.9
