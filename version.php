7da9202efa737aa70310e439acee354e8d7f00e1
David Monllaó <david.monllao@gmail.com>
2012-05-21 12:41:32 +0200

    CONTRIB-3656 - Support for journal 1.9 backups

--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?php
 
-$module->version  = 2012041900;
+$module->version  = 2012052100;
 $module->requires = 2010112400;  // Requires this Moodle version
-$module->release = '1.3 (Build: 2012041900)';
+$module->release = '1.4 (Build: 2012052100)';
 $module->maturity = MATURITY_STABLE;
 $module->cron     = 60;
 
