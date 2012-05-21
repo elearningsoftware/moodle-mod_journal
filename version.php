96b7ec00310eddbe73060c9d3862f3909f477d8e
David Monllaó <david.monllao@gmail.com>
2012-05-21 12:48:19 +0200

    CONTRIB-3656 - Changed required moodle version

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 
 $module->version  = 2012052100;
-$module->requires = 2010112400;  // Requires this Moodle version
+$module->requires = 2011070100;  // Moodle 2.1 (because of 1.9 course restore)
 $module->release = '1.4 (Build: 2012052100)';
 $module->maturity = MATURITY_STABLE;
 $module->cron     = 60;
