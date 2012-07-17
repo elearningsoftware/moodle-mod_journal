bd2c46f473b555efe4a3c606266682373c8bb89e
David Monllaó <david.monllao@gmail.com>
2012-07-17 22:00:18 +0800

    CONTRIB-3735 mod_journal Adding addinstance capability

--- db/access.php
+++ db/access.php
@@ -23,6 +23,18 @@ $capabilities = array(
             'teacher' => CAP_ALLOW,
             'editingteacher' => CAP_ALLOW
         )
-    )
+    ),
     
+    'mod/journal:addinstance' => array(
+        'riskbitmask' => RISK_XSS,
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+        'clonepermissionsfrom' => 'moodle/course:manageactivities'
+    )
+
 );
--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?php
 
-$module->version  = 2012052100;
-$module->requires = 2011070100;  // Moodle 2.1 (because of 1.9 course restore)
-$module->release = '1.4 (Build: 2012052100)';
+$module->version  = 2012071700;
+$module->requires = 2011061700;  // Moodle 2.3
+$module->release = '1.5 (Build: 2012071700)';
 $module->maturity = MATURITY_STABLE;
 $module->cron     = 60;
 
