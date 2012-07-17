42f15d80303db5931c44842de0bde7a8f4f7d153
David Monllaó <david.monllao@gmail.com>
2012-07-17 22:24:52 +0800

    CONTRIB-3735 - Correcting version number

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 
 $module->version  = 2012071700;
-$module->requires = 2011061700;  // Moodle 2.3
+$module->requires = 2011062500;  // Moodle 2.3
 $module->release = '1.5 (Build: 2012071700)';
 $module->maturity = MATURITY_STABLE;
 $module->cron     = 60;
