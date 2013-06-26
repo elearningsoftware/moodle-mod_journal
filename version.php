1047d8c394416b06f00ce3e7868af86be117fcf3
David Monllao <david.monllao@gmail.com>
2013-06-26 21:12:30 +0800

    Bump 2.5 version

--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?php
 
-$module->version  = 2013022700;
-$module->requires = 2012120300;  // Moodle 2.4
-$module->release = '1.8.1 (Build: 2013022700)';
+$module->version  = 2013062600;
+$module->requires = 2013051400;  // Moodle 2.5
+$module->release = '1.9.2 (Build: 2013062600)';
 $module->maturity = MATURITY_STABLE;
 $module->cron     = 60;
 
