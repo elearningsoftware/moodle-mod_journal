166813ebcb6a16d0a1925c968fcd14ab61ebf4a0
David Monllao <davidm@moodle.com>
2013-09-18 10:44:48 +0800

    Bump version

--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?php
 
-$module->version  = 2013062600;
+$module->version  = 2013091800;
 $module->requires = 2012120300;  // Moodle 2.4
-$module->release = '1.8.2 (Build: 2013062600)';
+$module->release = '1.8.3 (Build: 2013091800)';
 $module->maturity = MATURITY_STABLE;
 $module->cron     = 60;
 
