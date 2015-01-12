8adc46a9930f54126066cde18bbddbc8f51d1500
David Monllao <davidm@moodle.com>
2015-01-12 13:14:35 +0800

    New Moodle 2.5 version

--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?php
 
-$module->version  = 2013112800;
+$module->version  = 2015011200;
 $module->requires = 2013051400;  // Moodle 2.5
-$module->release = '1.9.4 (Build: 2013112800)';
+$module->release = '1.9.5 (Build: 2015011200)';
 $module->maturity = MATURITY_STABLE;
 $module->cron     = 60;
 
