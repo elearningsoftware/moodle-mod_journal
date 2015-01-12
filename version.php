e0a9e7888bb965e380ad7e212fd81e094a02a2e3
David Monllao <davidm@moodle.com>
2015-01-12 13:13:32 +0800

    New Moodle 2.6 version

--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?php
 
-$module->version  = 2013112800;
+$module->version  = 2015011200;
 $module->requires = 2013111800;  // Moodle 2.6
-$module->release = '1.10.4 (Build: 2013112800)';
+$module->release = '1.10.5 (Build: 2015011200)';
 $module->maturity = MATURITY_STABLE;
 $module->cron     = 60;
 
