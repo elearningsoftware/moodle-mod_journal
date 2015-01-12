401931dd1a6b03a42eb6ba9016a1e70e6a5149bf
David Monllao <davidm@moodle.com>
2015-01-12 13:12:06 +0800

    New Moodle 2.7 version

--- version.php
+++ version.php
@@ -1,8 +1,14 @@
 <?php
 
-$plugin->version  = 2014072100;
+// The version change from now will just be in
+// the last NN increasing it one by one. All lower
+// major versions version values will be lower
+// than this. Only when master becomes a stable
+// version the version value will be changed for
+// time() date.
+$plugin->version  = 2015011201;
 $plugin->requires = 2014050800;  // Moodle 2.7
-$plugin->release = '27.2 (Build: 2014072100)';
+$plugin->release = '27.3 (Build: 2015011201)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->cron     = 60;
 
