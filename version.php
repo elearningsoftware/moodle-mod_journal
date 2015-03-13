8b7d73177bf7e0c7befa5c8c068b4fcad459888f
David Monllao <davidm@moodle.com>
2015-03-13 14:07:48 +0800

    Version bump

--- version.php
+++ version.php
@@ -1,8 +1,9 @@
 <?php
 
-$module->version  = 2015011200;
+$module->component = 'mod_journal';
+$module->version  = 2015011201;
 $module->requires = 2013111800;  // Moodle 2.6
-$module->release = '1.10.5 (Build: 2015011200)';
+$module->release = '1.26.1 (Build: 2015011201)';
 $module->maturity = MATURITY_STABLE;
 $module->cron     = 60;
 
