c2c98ae46c2d1fd888c03d13b07810c6aa377c39
David Monllao <davidm@moodle.com>
2015-03-13 14:06:03 +0800

    Version bump

--- version.php
+++ version.php
@@ -6,8 +6,9 @@
 // than this. Only when master becomes a stable
 // version the version value will be changed for
 // time() date.
-$plugin->version  = 2015011300;
+$plugin->component = 'mod_journal';
+$plugin->version  = 2015011301;
 $plugin->requires = 2014111000;  // Moodle 2.8
-$plugin->release = '28.1 (Build: 2015011300)';
+$plugin->release = '1.28.1 (Build: 2015011301)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->cron     = 60;
