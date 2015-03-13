fc2ddd73b0dda215f0bdc62e58045fcf76d2fff3
David Monllao <davidm@moodle.com>
2015-03-13 14:20:39 +0800

    Adding the component as will be required in 3.0

--- version.php
+++ version.php
@@ -6,6 +6,7 @@
 // than this. Only when master becomes a stable
 // version the version value will be changed for
 // time() date.
+$plugin->component = 'mod_journal';
 $plugin->version  = 2015011300;
 $plugin->requires = 2014111000;  // Moodle 2.8
 $plugin->release = '28.1 (Build: 2015011300)';
