a437ecfcba42b8c3e52005a0d89474d1def065b6
David Monllao <davidm@moodle.com>
2016-01-08 09:14:48 +0800

    Bump version

--- version.php
+++ version.php
@@ -4,8 +4,8 @@
 // the current date. Otherwise we just increase the last NN by one.
 
 $plugin->component = 'mod_journal';
-$plugin->version  = 2015120400;
+$plugin->version  = 2015120401;
 $plugin->requires = 2015111600;  // Moodle 3.0
-$plugin->release = '30.0 (Build: 2015120400)';
+$plugin->release = '30.1 (Build: 2015120401)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->cron     = 60;
