91dd54b6591039fb4ed666b88b64e816419aae97
Adrian Sarmas <adrian.sarmas@yahoo.com>
2018-01-22 11:44:14 +0200

    Bump version number

--- version.php
+++ version.php
@@ -20,7 +20,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_journal';
-$plugin->version  = 2017121100;
+$plugin->version  = 2018012200;
 $plugin->requires = 2017111300;  /* Moodle 3.4 */
 $plugin->release = '34.0 (Build: 2017121100)';
 $plugin->maturity = MATURITY_STABLE;
