7e1888edb671abcf2f3023b9eb87c6d09b68165c
paulneag <paul.neag@elearningsoftware.ro>
2019-01-15 10:28:22 +0200

    update version of moodle

--- version.php
+++ version.php
@@ -20,8 +20,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_journal';
-$plugin->version  = 2018110700;
-$plugin->requires = 2018051700;  /* Moodle 3.5 */
+$plugin->version  = 2019011500;
+$plugin->requires = 2018120302;  /* Moodle 3.6.2 */
 $plugin->release = '34.0 (Build: 2017121100)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->cron     = 60;
