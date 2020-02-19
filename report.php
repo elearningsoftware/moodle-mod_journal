26a9bbb0d65aefde7e1e449e97f9a2183dcfadd8
Luca Bösch <luca.boesch@bfh.ch>
2020-02-19 15:38:56 +0100

    Format the 'save all my feedback' button properly.

--- report.php
+++ report.php
@@ -189,7 +189,7 @@ if (!$users) {
     echo "<p class=\"feedbacksave\">";
     echo "<input type=\"hidden\" name=\"id\" value=\"$cm->id\" />";
     echo "<input type=\"hidden\" name=\"sesskey\" value=\"" . sesskey() . "\" />";
-    echo "<input type=\"submit\" value=\"".get_string("saveallfeedback", "journal")."\" />";
+    echo "<input type=\"submit\" value=\"".get_string("saveallfeedback", "journal")."\" class=\"btn btn-secondary m-t-1\"/>";
     echo "</p>";
     echo "</form>";
 }
