016a50a8b534a8ecb6f061ab1c30647ba984ce08
Adrian Sarmas <adrian.sarmas@yahoo.com>
2020-09-11 09:05:07 +0300

    Upgrade to Moodle 3.9

--- version.php
+++ version.php
@@ -20,8 +20,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_journal';
-$plugin->version  = 2020020700;
-$plugin->requires = 2013111800;  /* Moodle 2.6 */
-$plugin->release = '2.0.0 (Build: 2019061600)';
+$plugin->version  = 2020091100;
+$plugin->requires = 2017111300;  /* Moodle 3.4 */
+$plugin->release = '2.1.0 (Build: 2020091100)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->cron     = 60;
