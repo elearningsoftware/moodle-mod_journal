ec6807c7e0cc9a1789ffec5f462611d8c107932d
Andrew Normore <andrewnormore@catalyst-ca.net>
2023-10-11 10:03:09 -0400

    get_users_in_context: more accurate sql query

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -120,16 +120,17 @@ class provider implements
         // Find users with journal entries.
         $sql = "
             SELECT je.userid
-                FROM {journal_entries} je
-                JOIN {modules} m
-                    ON m.name = :journal
-                JOIN {course_modules} cm
-                    ON cm.instance = je.id
-                AND cm.module = m.id
-                JOIN {context} ctx
-                    ON ctx.instanceid = cm.id
-                AND ctx.contextlevel = :modulelevel
-                WHERE ctx.id = :contextid";
+              FROM {journal} j
+              JOIN {journal_entries} je ON je.journal = j.id
+              JOIN {modules} m
+                ON m.name = :journal
+              JOIN {course_modules} cm
+                ON cm.instance = j.id
+               AND cm.module = m.id
+              JOIN {context} ctx
+                ON ctx.instanceid = cm.id
+               AND ctx.contextlevel = :modulelevel
+             WHERE ctx.id = :contextid";
         $params = ['journal' => 'journal', 'modulelevel' => CONTEXT_MODULE, 'contextid' => $context->id];
 
         $userlist->add_from_sql('userid', $sql, $params);
