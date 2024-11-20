ab855f515d7ee03a04dba28d800488e6e1675a94
Adrian Sarmas <adrian.sarmas@yahoo.com>
2024-11-20 08:59:28 +0200

    Add missing language string

--- lang/en/journal.php
+++ lang/en/journal.php
@@ -59,6 +59,10 @@ $string['incorrectuserid'] = 'Selected user ID is incorrect';
 $string['journal:addentries'] = 'Add journal entries';
 $string['journal:addinstance'] = 'Add a new journal';
 $string['journal:manageentries'] = 'Manage journal entries';
+$string['journalmail'] = '{$a->teacher} has posted some feedback on your
+journal entry for \'{$a->journal}\'
+You can see it appended to your journal entry:
+    {$a->url}';
 $string['journalmailhtml'] = '{$a->teacher} has posted some feedback on your
 journal entry for \'<i>{$a->journal}</i>\'<br /><br />
 You can see it appended to your <a href="{$a->url}">journal entry</a>.';
