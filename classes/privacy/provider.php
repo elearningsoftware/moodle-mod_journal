ee8dee89259cc475ac8979b5adc399ff3d6d9dce
Adrian Sarmas <adrian.sarmas@yahoo.com>
2025-12-06 10:03:01 +0200

    Format code according to Moodle guidelines including order of implements

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -29,10 +29,13 @@ defined('MOODLE_INTERNAL') || die();
 use context;
 use context_module;
 use core_privacy\local\metadata\collection;
+use core_privacy\local\metadata\provider as metadata_provider;
 use core_privacy\local\request\approved_contextlist;
 use core_privacy\local\request\approved_userlist;
 use core_privacy\local\request\contextlist;
+use core_privacy\local\request\core_userlist_provider;
 use core_privacy\local\request\helper;
+use core_privacy\local\request\plugin\provider as plugin_provider;
 use core_privacy\local\request\transform;
 use core_privacy\local\request\userlist;
 use core_privacy\local\request\writer;
@@ -47,9 +50,9 @@ require_once($CFG->dirroot . '/mod/journal/lib.php');
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class provider implements
-    \core_privacy\local\metadata\provider,
-    \core_privacy\local\request\core_userlist_provider,
-    \core_privacy\local\request\plugin\provider {
+    core_userlist_provider,
+    metadata_provider,
+    plugin_provider {
     /**
      * Returns metadata.
      *
