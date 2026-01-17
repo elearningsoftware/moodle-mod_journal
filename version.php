cb1211bc0814059a1114c7cfb4a337086d6a0345
Adrian Sarmas <adrian.sarmas@yahoo.com>
2026-01-17 11:48:09 +0200

    Bump version number

--- version.php
+++ version.php
@@ -27,5 +27,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'mod_journal';
 $plugin->version = 2026011800;
 $plugin->requires = 2022041900;  /* Moodle 4.0 */
-$plugin->release = '5.0.0 (Build: 2025121400)';
+$plugin->release = '5.1.0 (Build: 2026011800)';
 $plugin->maturity = MATURITY_STABLE;
